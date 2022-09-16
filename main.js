b00faf11b91c9b06d534c1cedf60ac1d6a6612ec
BrokenHeart0322 <mmaafia018@gmail.com>
2022-09-16 10:55:34 +0500

    nearly-all

---
+++ main.js
@@ -0,0 +1,195 @@
+// var student = {
+//     name: "Man",
+//     age: 16,
+//     score: 142,
+//     married: true,
+// };
+
+// if(student.score > 100){
+//     console.log("Prinyatsan !");
+// }
+
+// if(student.married == true){
+//     console.log("Voy bechara");
+// } else {
+//     console.log("Oqi mazzaqb")
+// }
+
+// function logName(x, y) {
+//     console.log(x+y/2)
+// }
+
+// logName(14, 15)
+// logName(11,22)
+
+
+
+
+// function addFvBook(bookName){
+//     if (bookName.includes("Great")){
+//         addFvBookTable.push(bookName)
+//     }
+// }
+// var addFvBookTable = []
+
+// 1-problem
+// var number = 7
+// if(number >= 0){
+//     console.log("musbat");
+// }else{
+//     console.log("- son");
+// }
+
+// 2,3-problem
+//  var number = 7
+//  if(number % 2 == 0){
+//     console.log("четный");
+//  }else{
+//     console.log("нечетный");
+//  }
+
+// 4-problem
+// var number = 2
+// var number2 = 3
+// if(number > 0 && number2 > 0){
+//    console.log("eee");
+// }else{
+//    console.log("sss");
+// }
+
+// 5-problem
+// var a = 7
+// var b = 5
+// var c = 3
+// if( a > b && b > c){
+//    console.log("a > b, b > c");
+// }else{
+//    console.log(false);
+// }
+
+// 6-problem
+// var a = 7
+// var b = 5
+// var c = 3
+// if(a > b && c <b){
+//    console.log("b ortada");
+// }else{
+//    console.log(false);
+// }
+
+// 7-problem
+// var a = 8
+// var b = 4
+// if(a % 2 == 0 && b % 2 == 0){
+//    console.log("четные");
+// }else{
+//    console.log("нечетные");
+// }
+
+// 8-problem
+// var a = 7
+// var b = 5
+// var c = 3
+// var d = 9
+// if(a,b,c,d > 0){
+//    console.log("all musbat");
+// }else{
+//    console.log("orasida manfiy bor");
+// }
+
+// 9-problem
+// var a = -5
+// var b = -7
+// var c = 9
+// if(a,b < 0 && c>0){
+//     console.log("boshidi 2-tasi musbat");
+// }else{
+//     console.log("false");
+// }
+
+// 10-problem
+// let days = [
+//     "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+// ]
+// let num = prompt("print days of week");
+
+// if(num > 0 && num < 8){
+//   console.log(days[num - 1])
+// }
+// else alert("loh")
+
+// 12-problem
+// var a = -6
+// if(a > 0){
+//     console.log(a+1);
+// }else{
+//     console.log(a-1);
+// }
+
+// 13-problem
+// var a = -7
+// if(a > 0){
+//     console.log(a*3);
+// }else{
+//     console.log(a+2);
+// }
+
+// 14-problem
+// var a = 7
+// var b = 9
+// if(a > b){
+//     console.log("first number > than second");
+// }else{
+//     console.log("second number > than first");
+// }
+
+// 15-problem
+// var a = 5
+// var b = 7
+// var c = 9
+// if(a > b && a > c){
+//     console.log("1-number en kotta");
+// }else if(b > a && b > c){
+//     console.log("2-number en kotta");
+// }else if(c > a && c > b){
+//     console.log("3-number en kotta");
+// }
+
+// 16-problem
+// var a = 5
+// var b = 7
+// var c = 9
+// if(a < b && a < c){
+//     console.log("1-number en kichkinasi");
+// }else if(b < a && b < c){
+//     console.log("2-number en kichkinasi");
+// }else if(c < a && c < b){
+//     console.log("3-number en kichkinasi");
+// }
+
+// 17-problem
+// var score = 0
+// if(score == 1 || score == 0){
+//     console.log("institutdan haydaldin");
+// }else if(score == 2){
+//     console.log("perezdachaga qoldin");
+// }else if(score == 3){
+//     console.log("sanga 250 000 so'm ajratldi");
+// }else if(score == 4){
+//     console.log("sanga 450 000 so'm ajratldi");
+// }else if(score == 5){
+//     console.log("sanga 550 000 so'm ajratldi");
+// }
+
+// 19-problem
+// var a = 7
+// if(a > 0 && a % 2 == 0){
+//     console.log("bu juft musbat son");
+// }else if(a < 0 && a % 2 == 0){
+//     console.log("bu juft manfi son");
+// }else if(a > 0 && a % 2 == 1){
+//     console.log("bu toq musbat son");
+// }else if(a < 0 && a % 2 == 1){
+//     console.log("bu toq manfi son");
+// }
+ 
